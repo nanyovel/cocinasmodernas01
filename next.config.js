db9136d437b2e05390eb0f870e754026f824e897
Jose Perez <jperezjimenez06gmail.com>
2026-04-23 16:51:58 -0400

    corrgiendo errores de indexacion

--- next.config.js
+++ next.config.js
@@ -19,6 +19,21 @@ const nextConfig = {
       },
     ];
   },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "cocinasmodernas01.vercel.app" }],
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex, nofollow",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
